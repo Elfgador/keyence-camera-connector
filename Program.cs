91f703c6776a585bf20ebcf808e1128d367143c6
elfgador <alex.moreno@live.fr>
2025-03-06 11:26:29 +0100

    fix ip

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using CameraConnector.Command;
 class Program
 {
     static readonly string CameraIp = "192.168.10.99";
-    static readonly string SimulatorIp = "172.17.0.2";
+    static readonly string SimulatorIp = "192.168.0.10";
 
     static async Task Main()
     {
         try
         {
             // Connection
-            await EtherNetIPClient.Connect(CameraIp);
+            await EtherNetIPClient.Connect(SimulatorIp);
 
             // Run program
             byte[] runCommand = Command.CreateCommand(1, CommandsNumber.RUN, []);
